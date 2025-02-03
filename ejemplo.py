73018fa291c79537edfae945d867326ec54b8e0b
WeedLaWeed <165701580+WeedLaWeed@users.noreply.github.com>
2025-02-03 21:59:48 +0100

    Add files via upload

--- ejemplo.py
+++ ejemplo.py
@@ -1,23 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL de la página de actualidad de Wikipedia
-url = "https://es.wikipedia.org/wiki/Portal:Actualidad"
+url = "https://es.wikipedia.org/wiki/Wikipedia:Portada"
+response=requests.get(url)
 
-# Hacemos la petición a la página
-response = requests.get(url)
+soup = BeautifulSoup(response.text,"html.parser")
 
-# Verificamos que la petición fue exitosa (código 200)
-if response.status_code == 200:
-    # Parseamos el contenido HTML
-    soup = BeautifulSoup(response.text, "html.parser")
+novedades = soup.find(id='main-cur')
 
-    # Buscamos todos los títulos dentro de los encabezados <h2>, <h3>, etc.
-    titulos = soup.find_all(["h2", "h3"])
-
-    print("Títulos de actualidad en Wikipedia:")
-    for titulo in titulos:
-        print("-", titulo.text.strip())
-
-else:
-    print(f"Error al acceder a la página. Código de estado: {response.status_code}")
+for li in novedades.find_all("li"): 
+    print(li.text, end=" "+"\n")
